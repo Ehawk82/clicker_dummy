86d2e3501c28d62f481bf76e61e567ad5168c6a5
unknown <mrehawk82@gmail.com>
2021-09-18 12:35:10 -0400

    scripting

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -1,5 +1,28 @@
 const init = () => {
-	console.log(helloJS)
+	var gD = parseLS("game_data");
+
+	if(!gD || gD === null) {
+		LSinit("game_data",gameData);
+
+		gD = parseLS("game_data");
+		bodyBulld(gD);
+	} else {
+		bodyBulld(gD);
+	}
+},
+bodyBulld = (gD) => {
+	const container = createEle("div");
+
+	//
+	for (var i = 0; i < btn_names.length; i++) {
+		const btns = createEle("button");
+
+		btns.innerHTML = btn_names[i];
+		container.append(btns);
+	}
+	container.className = "container";
+
+	body.append(container);
 };
 window.onload = () => {
 	init();
