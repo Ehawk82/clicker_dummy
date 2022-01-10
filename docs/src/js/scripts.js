5de32eb8e47132e75770affd635ce55debb97a3b
unknown <mrehawk82@gmail.com>
2022-01-10 12:35:00 -0500

    more UI added

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -1,31 +1,89 @@
 const init = () => {
-	bodyBulld();
+	const gd = parseLS("gameData");
+	if(!gd){
+		LSinit("gameData", gamedata);
+		const gd = parseLS("gameData");
+		bodyBulld(gd);
+	}else{
+		bodyBulld(gd);
+	}
 },
-bodyBulld = () => {
+bodyBulld = (gd) => {
 	const container = createEle("div"),
 	      logger = createEle("div"),
 	      settings = createEle("div"),
-	      eggsCell = createEle("span"),
+	      level = createEle("span"),
+	      money = createEle("span"),
+	      playBox = createEle("div"),
+	      btnBox = createEle("div"),
+	      arena = createEle("div"),
 	      mat = createEle("div");
 
 	//
-	eggsCell.innerHTML = "EGGS: ";
-	eggsCell.className = "eggsCell w3-white w3-card-2 w3-padding";
+	level.innerHTML = "LEVEL: " + gd.level;
+	level.className = "level w3-white w3-half w3-center";
 
-	settings.append(eggsCell);
-	settings.className = "settings w3-container w3-grey w3-card-2 w3-padding";
+	money.innerHTML = "💲" + gd.money;
+	money.className = "money w3-white w3-half w3-center";
 
-	mat.innerHTML = "no light";
-	mat.className = "mat w3-contain w3-white w3-padding w3-card-2";
+	settings.append(level,money);
+	settings.className = "settings w3-contain";
+
+	playBox.innerHTML = "playbox";
+	playBox.className = "playBox w3-card-4 w3-container w3-white w3-half w3-margin";
+
+	for (var i = 0; i < btns.length; i++) {
+		const btnHolder = createEle("p"),
+			  addBtnLabel = createEle("div"),
+			  addBtnTitle = createEle("span"),
+			  addBtnDetails = createEle("div"),
+			  addBtn = createEle("button");
+		//
+		for (var k = 0; k < 3; k++) {
+			const detailRow = createEle("div");
+
+			detailRow.innerHTML = "row" + k;
+			detailRow.className = "detailRow w3-container w3-card-2";
+
+			addBtnDetails.append(detailRow);
+		}
+		
+		addBtnDetails.className = "addBtnDetails w3-contain w3-monarch";
+
+		addBtnTitle.innerHTML = btns[i];
+		addBtnTitle.className = "addBtnTitle w3-margin"
+
+		addBtnLabel.append(addBtnTitle,addBtnDetails);
+		addBtnLabel.className = "addBtnLabel w3-contain";
+
+		addBtn.innerHTML = "+";
+		addBtn.className = "addBtn w3-button w3-hover-light-green w3-green";
+
+		btnHolder.append(addBtn,addBtnLabel);
+		btnHolder.className = "btnHolder w3-contain w3-card-2";
+        
+        if(i >= gd.level) {
+        	btnHolder.classList.add("w3-hide");
+        } 
+
+		btnBox.append(btnHolder);
+	}
+    // btnBox.innerHTML = "&nbsp;";
+	btnBox.className = "btnBox w3-card-4 w3-container w3-white w3-half w3-margin";
+
+	arena.append(btnBox,playBox);
+	arena.className = "arena w3-card-2 w3-padding w3-green";
+
+	mat.append(arena);
+	mat.className = "mat w3-contain w3-black w3-padding w3-card-2";
 
 	container.append(settings,mat);
-	container.className = "container w3-container w3-white w3-padding";
+	container.className = "container w3-container w3-white";
 
 	logger.innerHTML = " logger";
-	logger.className = "logger w3-white w3-bottom w3-text-grey";
+	logger.className = "logger w3-black w3-bottom w3-text-white";
 
 	body.append(container,logger);
-	body.className = "w3-container";
 };
 window.onload = () => {
 	init();
--- docs/src/js/stuff.js
+++ docs/src/js/stuff.js
@@ -1,8 +1,7 @@
 var gamedata = {
-	eggs:1,
-	larvae:0,
-	cats:0,
-	butter:0,
-	queen:0,
-	migrators:0
+	money:100,
+	level:1
 };
+
+var btns = ["Goal 1","Goal 2","Goal 3","Goal 4","Goal 5","Goal 6","Goal 7","Goal 8","Goal 9","Goal 10","Goal 11","Goal 12","Goal 13","Goal 14","Goal 15","Goal 16","Goal 17","Goal 18","Goal 19","Goal 20"];
+
